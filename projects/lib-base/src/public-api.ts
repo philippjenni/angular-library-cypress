449028fb3b97d1d75f757260292806547403ea97
Phliipp Jenni <philipp.jenni@jnetwork.ch>
2023-11-12 14:09:21 +0100

    feat: Move base class to own lib

--- projects/lib-base/src/public-api.ts
+++ projects/lib-base/src/public-api.ts
@@ -2,4 +2,5 @@
  * Public API Surface of lib-base
  */
 
+export { LibBase } from './lib/lib.base.component';
 export * from './lib/lib-base.module';
--- projects/lib-component/src/lib/lib.component.ts
+++ projects/lib-component/src/lib/lib.component.ts
@@ -1,11 +1,10 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { LibBase } from './lib.base.component';
+import { LibBase } from 'lib-base';
 
 @Component({
   selector: 'lib-libcomponent',
   templateUrl: './lib.component.html',
 })
 export class LibComponent extends LibBase {
-
 
 }
